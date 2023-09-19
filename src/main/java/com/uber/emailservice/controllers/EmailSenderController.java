5a3dd0cc9a4824ff9c902430ff32cda32ea0f92f
robsonrrn <robsonrrn@gamil.com>
2023-09-19 12:13:37 -0300

    feat: create email service controller

---
+++ src/main/java/com/uber/emailservice/controllers/EmailSenderController.java
@@ -0,0 +1,30 @@
+package com.uber.emailservice.controllers;
+
+import com.uber.emailservice.application.EmailSenderService;
+import com.uber.emailservice.core.EmailRequest;
+import com.uber.emailservice.core.exceptions.EmailServiceException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/email")
+public class EmailSenderController {
+    private final EmailSenderService emailSenderService;
+
+    @Autowired
+    public  EmailSenderController(EmailSenderService emailService){
+        this.emailSenderService = emailService;
+    }
+
+    @PostMapping()
+    public ResponseEntity<String> sendEmail(@RequestBody EmailRequest request){
+        try{
+            this.emailSenderService.sendEmail(request.to(), request.subject(), request.body());
+            return  ResponseEntity.ok("email sent successfully");
+        } catch (EmailServiceException ex){
+            return  ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("error while sending email");
+        }
+    }
+}
